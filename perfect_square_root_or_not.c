32e4707969b3672c11857eabf0d3bb52cd9ecee9
JAGADEESH206 <147473050+JAGADEESH206@users.noreply.github.com>
2024-03-17 20:46:44 +0530

    #include<stdio.h> #include<math.h> int main() {     int n;     float d;     scanf("%d",&n);     d=sqrt(n);     if(ceil(d)==floor(d))     printf("True");     else     printf("False"); }

---
+++ perfect_square_root_or_not.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n; 
+    float d;
+    scanf("%d",&n);
+    d=sqrt(n); 
+    if(ceil(d)==floor(d))
+    printf("True"); 
+    else 
+    printf("False"); }
